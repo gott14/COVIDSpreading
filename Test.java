4bd171a886598e86d0e4186a842cdb46a83a2192
Griffin Ott <gott@bowdoin.edu>
2020-10-27 18:30:33 -0400

    implemented testing to check that event grouping works

--- Test.java
+++ Test.java
@@ -10,10 +10,16 @@ public class Test
    public static void main (String [] args)
    { 
        State maine = new State(100);
-       maine.people.get(0).infect();
-       while(true)
+       for(int i = 0; i < 10; i++)
+       {
+           maine.getPeople().get(i).infect();
+       }
+       for(int i = 0; i < 100; i++)
        {
          System.out.println(maine.numInfected());
+         for(int k = 0; k < 4; k++) {
+         maine.executeEvent(100, 1.0); }
+         maine.executeEvent(10, 10.0);
          maine.advanceDay();
        }
    }
