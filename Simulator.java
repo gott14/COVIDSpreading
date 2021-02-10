0c1b0e3273ef0fd677f0f29815f27e1bd07d928b
Griffin Ott <gott@bowdoin.edu>
2021-02-10 10:22:28 -0500

    added instance variable for events to be simulated

--- Simulator.java
+++ Simulator.java
@@ -17,10 +17,7 @@ public class Simulator
     private static int EVENT_CAP = 50; //max people at one high risk event
     private int daysToSim;
     
-    private static double LOW_RISK_MULT = 0.3; //multiplier for low risk events
-    private static double MED_RISK_MULT = 1.0;
-    private static double HIGH_RISK_MULT = 4.0;
-    
+    private ArrayList<Event> events;
     
     public Simulator(int population, int initialCases, int days)
     {
@@ -33,6 +30,11 @@ public class Simulator
         }
     }
     
+    public Simulator(int days, String state)
+    {
+        daysToSim = days;
+        
+    }
     public static void setOpenness(double n)
     {
         OPENNESS = n;
@@ -53,21 +55,15 @@ public class Simulator
         return state;
     }
     
+    public void determineEvents(int initCases, int finalCases) //populates inst var with events that would be needed per week to
+                                                               //cause the given rise in cases
+    {
+        
+    }
+    
     public void run()
     {
-        for(int d = 0; d < daysToSim; d++)
-        {
-            for(int i = 0; i < 1; i++)
-                state.executeEvent(state.getPopulation() / 2, LOW_RISK_MULT);
-            for(int k = 0; k < 1; k++)
-                state.executeEvent(state.getPopulation() / 10, MED_RISK_MULT);
-            for(int j = 0; j < 1; j++)
-                state.executeEvent(state.getPopulation() / 2, HIGH_RISK_MULT);
-        }
         
-        System.out.println("Currently infected: " + state.numInfected());
-        System.out.println("Total infected: " + state.numTotalCases());
-        System.out.println("Total deaths: " + state.numDeaths());
     }
     
     public static void main(String [] args)
